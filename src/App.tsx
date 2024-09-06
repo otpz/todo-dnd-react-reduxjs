baaaf100b0599535e2e13a3570b16b9bc7a0dbc8
Osman Topuz <osmantopuz98@gmail.com>
2024-09-06 14:52:23 +0300

    added types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
     <div className="container">
       
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/types/Task.ts
@@ -0,0 +1,8 @@
+export type Task = {
+    id: number,
+    listId: number,
+    title: string,
+    isActive: boolean,
+    isDeleted: boolean,
+    createdDate: string
+}
